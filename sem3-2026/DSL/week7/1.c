4fd05a62b0c4e811c10ce048b03b60cd75b5a477
TreasureH21 <baladityavs@gmail.com>
2026-08-31 04:40:35 +0530

    Create 1.c

---
+++ sem3-2026/DSL/week7/1.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+struct Stack
+{
+    char data[MAX];
+    int top;
+};
+
+void initialize(struct Stack *s)
+{
+    s->top = -1;
+}
+
+void push(struct Stack *s, char ch)
+{
+    if(s->top == MAX - 1)
+    {
+        printf("Stack Overflow\n");
+        return;
+    }
+
+    s->top++;
+    s->data[s->top] = ch;
+}
+
+char pop(struct Stack *s)
+{
+    char ch;
+
+    if(s->top == -1)
+    {
+        return '\0';
+    }
+
+    ch = s->data[s->top];
+    s->top--;
+
+    return ch;
+}
+
+int isPalindrome(char str[])
+{
+    struct Stack s;
+    int i;
+    char ch;
+
+    initialize(&s);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        push(&s, str[i]);
+    }
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        ch = pop(&s);
+
+        if(str[i] != ch)
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int isMatching(char expr[])
+{
+    struct Stack s;
+    int i;
+    char ch;
+
+    initialize(&s);
+
+    for(i = 0; expr[i] != '\0'; i++)
+    {
+        ch = expr[i];
+
+        if(ch == '(' || ch == '[' || ch == '{')
+        {
+            push(&s, ch);
+        }
+        else if(ch == ')' || ch == ']' || ch == '}')
+        {
+            if(s.top == -1)
+            {
+                return 0;
+            }
+
+            ch = pop(&s);
+
+            if((expr[i] == ')' && ch != '(') ||
+               (expr[i] == ']' && ch != '[') ||
+               (expr[i] == '}' && ch != '{'))
+            {
+                return 0;
+            }
+        }
+    }
+
+    if(s.top == -1)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int choice;
+    char str[MAX];
+
+    do
+    {
+        printf("\n\n----- STACK OPERATIONS -----\n");
+        printf("1. Check Palindrome\n");
+        printf("2. Check Matching Parentheses\n");
+        printf("3. Exit\n");
+
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                printf("Enter a string: ");
+                scanf("%s", str);
+
+                if(isPalindrome(str))
+                    printf("The string is a palindrome.\n");
+                else
+                    printf("The string is not a palindrome.\n");
+
+                break;
+
+            case 2:
+                printf("Enter an expression: ");
+                scanf("%s", str);
+
+                if(isMatching(str))
+                    printf("Parentheses are matching.\n");
+                else
+                    printf("Parentheses are not matching.\n");
+
+                break;
+
+            case 3:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice.\n");
+        }
+
+    } while(choice != 3);
+
+    return 0;
+}
